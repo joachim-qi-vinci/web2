a5400e27b5d6638d3e2aa70e53c0ecb2569449a9
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-10-17 12:25:28 +0200

    2.4.1 : run action within timer.

--- exercises/2.4.1/external-script.js
+++ exercises/2.4.1/external-script.js
@@ -7,7 +7,7 @@ btn.addEventListener("mouseover", startTimer);
 window.addEventListener("click", addClick);
 
 let timerID;
-const timerS = 10;
+const timerS = 5;
 const timerMs = timerS *1000;
 let cpmt= 0;
 
@@ -16,7 +16,7 @@ function startTimer(){
     text.innerText = "Commencons !";
     
     timerID = setTimeout(()=>{
-        text.innerText = "Game over, you failed";
+        text.innerText = "Game over, you did not click 10 times within 5s !";
     }, timerMs);
 }
 
@@ -24,7 +24,7 @@ function addClick(){
     const spend = new Date().getTime() - startTime.getTime();
     cpmt ++;
     if (cpmt === 10){
-        text.innerText = 'You are a professional you made it within ' + spend / 1000 + 's';
+        text.innerText = 'You win ! You clicked 10 times within ' + spend / 1000 + 's';
         clearTimeout(timerID);
     }
 }
