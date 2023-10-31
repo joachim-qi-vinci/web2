8a40c5f51e2d8a810ff696d288bbb379123e908f
joachim-qi-vinci <joachim.qi@student.vinci.be>
2023-10-31 19:12:14 +0100

    2.7 homepage

---
+++ exercises/2.4.2/external-script.js
@@ -0,0 +1,10 @@
+const divRouge = document.querySelector(".rouge");
+const divOrange = document.querySelector(".orange");
+const divVert = document.querySelector(".vert");
+
+const redColor = 'red';
+const orangeColor = 'orange';
+const vertColor = 'green';
+const noColor = '';
+
+
